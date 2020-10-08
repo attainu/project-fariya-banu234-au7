9527bf30bac54cc52445ebfb5ba075e9f8e4b809
Nagarjun-nagendra-au6 <nagarjun9499@gmail.com>
2020-10-08 20:25:32 +0530

    deployed

--- server/routes/deleteRoutes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var { validateToken } = require('../middleware/authenticate');
-var {userLogout} = require('../controllers/deleteControllers')
-var router = express.Router();
-
-router.delete("/logout",validateToken, userLogout);
-
-module.exports = router;
